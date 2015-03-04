a2586c823ba10f2f9975ea07a9a929e549885928
David Morales <david.morales0092@gmail.com>
2015-03-03 16:55:55 -0800

    Add ability to add files to index

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,12 +1,16 @@
 
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
@@ -21,14 +25,28 @@ public class Indexer {
 	 */
 	public static void main(String[] args) {
 		
-		String indexDir = "C:/Users/ASUS/Desktop/LUCENE";
-		File indexDirHandle = new File(indexDir);
-		if(!indexDirHandle.isDirectory())
+		String indexDir = "C:/Users/ASUS/Desktop/lucene-test";
+		String dataDir = "C:/Users/ASUS/Desktop/pages";
+		File indexDirFile = new File(indexDir);
+		File dataDirFile = new File(dataDir);
+		
+		// Verify that both directories are valid
+		if(!indexDirFile.isDirectory())
 		{
-			System.out.println("is not a valid directory");
+			System.out.println("Specified index location is not a valid directory");
 			return;
 		}
+		if(!dataDirFile.isDirectory())
+		{
+			System.out.println("Specified data location is not a valid directory");
+			return;
+		}
+		
+		// Create an index at indexDir using files from dataDir
 		Indexer indexer = new Indexer(indexDir);
+		indexer.addFilesFromDirectory(dataDir);
+		
+		System.out.println("Done!");
 	}
 	
 	private IndexWriter writer;
@@ -37,15 +55,14 @@ public class Indexer {
 	public Indexer(String indexDir)
 	{
 		writer = getIndexWriter(indexDir);
-		
 	}
 	
 	IndexWriter getIndexWriter(String indexDir)
 	{
 		try {
 			//Directory used to store index
-			File dirFileHandle = new File(indexDir);
-			Directory dir = FSDirectory.open(Paths.get(dirFileHandle.toURI()));
+			File indexDirFile = new File(indexDir);
+			Directory dir = FSDirectory.open(Paths.get(indexDirFile.toURI()));
 			
 			//Token analyzer for processing Documents
 			StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -56,10 +73,42 @@ public class Indexer {
 			
 			return new IndexWriter(dir, writerConfig);
 		} catch (IOException e) {
-			System.out.println("Index directory path is invalid.");
-			//System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 		//Return null if the IndexWriter was not set up properly
 		return null;
 	}
+	
+	public void addFilesFromDirectory(String dataDir)
+	{
+		//Get all Files in the dataDir directory
+		File dataDirFile = new File(dataDir);
+		File[] dataFiles = dataDirFile.listFiles();
+		
+		//Add each file to the index
+		for(File file : dataFiles)
+			indexFile(file);
+	}
+	
+	void indexFile(File file)
+	{
+		try {
+			Document doc = getDocument(file);
+			writer.addDocument(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	Document getDocument(File f) throws FileNotFoundException
+	{
+		//Construct the Document
+		Document doc = new Document();
+		doc.add(new TextField("contents", new FileReader(f)));
+		//TODO: ADD MORE FIELDS
+		return doc;
+	}
+	
+	
 }
